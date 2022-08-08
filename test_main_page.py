9b4044f4fc1c9fba45bb8641ee202ab03022afca
Alexander <alexanderponomarev777@gmail.com>
2022-08-08 11:29:47 +0300

    modified fiels, add new methods

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,12 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link) # инициализируем POM, передаем в конструктор экземпляра драйвера и url адрес
     page.open() # открываем страницу
     page.go_to_login_page() # переходим на страницу логина
-    page.should_be_login_link() # проверить ссылку на логин
+    login_page = LoginPage(browser, browser.current_url) # инициализируем POM
+    login_page.should_be_login_page() # проверить ссылку на логин
 
 # last string
