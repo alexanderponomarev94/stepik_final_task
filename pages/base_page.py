bef27988b18ba301cdcc7290eea7b03761d07f0b
Alexander <alexanderponomarev777@gmail.com>
2022-08-09 11:41:16 +0300

    add new method (go to basket page)

--- pages/base_page.py
+++ pages/base_page.py
@@ -11,7 +11,7 @@ class BasePage():
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
-        
+
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
@@ -37,6 +37,11 @@ class BasePage():
         link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
         link.click()
 
+    # перейти в корзину
+    def go_to_basket_page(self):
+        link = self.browser.find_element(*BasePageLocators.BASKET_LINK)
+        link.click()
+
     def open(self):
         self.browser.get(self.url)
 
@@ -44,7 +49,9 @@ class BasePage():
     def should_be_login_link(self):
         assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
-    
+    def should_be_basket_link(self):
+        assert self.is_element_present(*BasePageLocators.BASKET_LINK), "Basket link is not presented"
+
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
         x = alert.text.split(" ")[2]
