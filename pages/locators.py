a13771ebe4af115aaa0a15fc83420d5b17e03de6
Alexander <alexanderponomarev777@gmail.com>
2022-08-09 14:30:57 +0300

    add new locators in class Base and Login

--- pages/locators.py
+++ pages/locators.py
@@ -4,6 +4,7 @@ class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     BASKET_LINK = (By.CSS_SELECTOR, ".btn-group a.btn.btn-default ")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 class BasketPageLocators():
     MESSAGE_BASCET_EMPTY = (By.CSS_SELECTOR, "#content_inner>p")
@@ -15,6 +16,11 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    REGISTRATION_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTRATION_PASS1 = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTRATION_PASS2 = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTRATION_BUTTON = (By.CSS_SELECTOR, "[name = 'registration_submit']")
+
 
 class ProductPageLocators():
     ADD_BUTTON_PRODUCT_TO_BASCET = (By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
