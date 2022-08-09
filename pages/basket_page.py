73ecafb4a3a19c1304b77772086ed99020a7f848
Alexander <alexanderponomarev777@gmail.com>
2022-08-09 11:36:22 +0300

    add new PageOdject

---
+++ pages/basket_page.py
@@ -0,0 +1,17 @@
+from .base_page import BasePage
+from .locators import BasketPageLocators
+
+class BasketPage(BasePage):
+
+    # проверка, корзина пустая
+    def should_be_message_about_bascet_empty(self): 
+        assert self.is_element_present(*BasketPageLocators.MESSAGE_BASCET_EMPTY), "Message about basket empty is not presented"
+
+     # проверка, товаров в корзине нет
+    def should_not_be_product_in_basket(self):
+        assert self.is_not_element_present(*BasketPageLocators.BASKET_ITEMS), "Product in basket is presented, but should not be"
+
+    
+
+
+
